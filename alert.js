88e9d58cc71386f6c6d0b13600a3a2bcb017a273
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-12-31 05:53:16 +0100

    Understanding JavaScript

---
+++ alert.js
@@ -0,0 +1,18 @@
+// //this is the Ultimate JavaScript Tutorial
+
+// "use script";
+// let name = "John";
+// let admin = name;
+
+// // alert(admin);
+
+// // this is for the second task in the module
+// let ourPlanetName = "Earth"
+// let currentUserName = "John Doe"
+
+// alert (ourPlanetName + " " + currentUserName);
+
+let name = "ILya";
+// alert( `hello ${1}` );
+// alert( `hello ${"name"}` );
+alert( `hello ${name}` );
