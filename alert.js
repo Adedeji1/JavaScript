cde6360436ec1e0404e790136f20751abd392877
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-12-31 06:16:28 +0100

    understanding of JavaScript interactions

--- alert.js
+++ alert.js
@@ -1,18 +1,7 @@
-// //this is the Ultimate JavaScript Tutorial
+// let test = prompt("Test", '');
 
-// "use script";
-// let name = "John";
-// let admin = name;
+let test = prompt("what is your name?", "");
 
-// // alert(admin);
+// let name = confirm("Are you sure", );
 
-// // this is for the second task in the module
-// let ourPlanetName = "Earth"
-// let currentUserName = "John Doe"
-
-// alert (ourPlanetName + " " + currentUserName);
-
-let name = "ILya";
-// alert( `hello ${1}` );
-// alert( `hello ${"name"}` );
-alert( `hello ${name}` );
+alert(test);
