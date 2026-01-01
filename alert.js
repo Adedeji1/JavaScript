8102658ac9a56b14d7159aefdd925cb7d114aa6f
Adedeji1 <afolabiadedeji110@yahoo.com>
2026-01-01 08:24:30 +0100

    still on java Script basics

--- alert.js
+++ alert.js
@@ -10,8 +10,25 @@
 
 
 // This a type Conversion section
-let value = true;
-alert(typeof value);
+// let value = true;
+// alert(typeof value);
 
-value = String(value);
-alert(typeof value)
+// value = String(value);
+// alert(typeof value)
+
+
+//Basic Operators (MATHS)
+// let a = 1, b = 1;
+
+// let c = ++a;
+// let d = b++;
+// console.log( a, b)
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x);
+
+let a = +prompt("First Number?", 1);
+let b = +prompt("Second Number?", 2);
+
+alert(a + b);
