89d39d44946d1a735196d36d5bbb4aec9dca36d2
Adedeji1 <afolabiadedeji110@yahoo.com>
2026-01-03 01:03:07 +0100

    moving on to the Loops

--- task.js
+++ task.js
@@ -1,16 +1,18 @@
-let UserName = prompt("who is there?", '');
+// let UserName = prompt("who is there?", '');
 
-if (userName === 'Admin') {
-    let pass = prompt('password', '');
-    if (pass === 'TheMaster') {
-        alert("welcome");
-    } else if (pass === '' || pass === null) {
-        alert( "canceled")
-    } else {
-        alert ("wrong password")
-    }
-} else if (userName === '' || userName === null) {
-  alert( 'Canceled' );
-} else {
-  alert( "I don't know you" );
-}
+// if (userName === 'Admin') {
+//     let pass = prompt('password', '');
+//     if (pass === 'TheMaster') {
+//         alert("welcome");
+//     } else if (pass === '' || pass === null) {
+//         alert( "canceled")
+//     } else {
+//         alert ("wrong password")
+//     }
+// } else if (userName === '' || userName === null) {
+//   alert( 'Canceled' );
+// } else {
+//   alert( "I don't know you" );
+// }
+
+// welcome to loops "the begining of JavaScript"
